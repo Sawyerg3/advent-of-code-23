14021971df0aa3bf2d919f26c176dcce56bff045
Sawyer Gitzel <sawyerg333@gmail.com>
2023-12-01 18:00:34 -0700

    Day one

--- day1/day1.py
+++ day1/day1.py
@@ -6,27 +6,13 @@ def lineValute(line):
         if line[x].isdigit():
             value = value + line[x]
         x += 1
-    # print("value:" , value)
     x = -1
-    # print(-x)
 
     while len(value) == 1  and x >= -chars:
-        # print("loop")
-        # print(line[x])
         if line[x].isdigit():
             value = value + line[x]
         x -= 1
 
-
-    '''
-    while len(value) == 1  and (chars - y != x):
-        # print("loop")
-        print(line[chars - y])
-        if line[chars - y].isdigit():
-            value = value + line[chars - y]
-        y += 1 '''
-    
-    print(value)
     return int(value)
 
 
@@ -34,20 +20,13 @@ def main():
     file = open("input.txt")
     # file = open("test.txt")
     line = file.readline()
-    # line = file.readline()
-    # line = file.readline()
-
 
     sum = 0
     while line != '': # EOF
         sum += lineValute(line)
-        # print('Reading:', line)
         line = file.readline()
 
     print(sum)
-    # calibrationDoc = file.read()
-    # print(calibrationDoc)
-
     return
 
 main()
